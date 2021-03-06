405045540be19249789efb398a27df509dd61258
Roman Sinclair <romanio412@gmail.com>
2021-03-06 08:37:00 +0000

    Update dotnet-setup.ps1

--- CI Scripts/dotnet-setup.ps1
+++ CI Scripts/dotnet-setup.ps1
@@ -5,6 +5,8 @@ $sdkExists = $false
 # https://dotnetcli.azureedge.net/dotnet/Sdk/5.0.200/dotnet-sdk-5.0.200-win-x64.exe
 # https://dotnetcli.azureedge.net/dotnet/aspnetcore/Runtime/5.0.3/aspnetcore-runtime-5.0.3-win-x64.exe
 
+# https://download.visualstudio.microsoft.com/download/pr/dff39ddb-b399-43c5-9af0-04875134ce04/1c449bb9ad4cf75ec616482854751069/dotnet-hosting-5.0.3-win.exe
+
 try {
     if(Get-Command dotnet) { $dotnetExists = $true }
 } 
@@ -20,7 +22,16 @@ if ($dotnetExists) {
     catch { }
 }
 
-if (!$sdkExists) { wget "https://dotnetcli.azureedge.net/dotnet/Sdk/5.0.200/dotnet-sdk-5.0.200-win-x64.exe" -outfile "dotnet-setup.exe";
+if (!$sdkExists) { 
+    wget "https://dotnetcli.azureedge.net/dotnet/Sdk/5.0.200/dotnet-sdk-5.0.200-win-x64.exe" -outfile "dotnet-setup.exe";
     $pathvargs = { .\dotnet-setup.exe /install /quiet /norestart };
     Invoke-Command -ScriptBlock $pathvargs
 }
+
+$runtimeExists = Get-WebGlobalModule | where-object { $_.name.ToLower() -eq "aspnetcoremodule" }
+if (!$runtimeExists) {
+    wget "https://download.visualstudio.microsoft.com/download/pr/dff39ddb-b399-43c5-9af0-04875134ce04/1c449bb9ad4cf75ec616482854751069/dotnet-hosting-5.0.3-win.exe" -outfile "dotnet-hosting-setup.exe";
+    $pathvargs = { .\dotnet-hosting-setup.exe /install /quiet /norestart };
+    Invoke-Command -ScriptBlock $pathvargs
+}
+catch { }
