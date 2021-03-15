cbab4219f8045888a1cd461b5d6cc63995f9a490
Roman Sinclair <romanio412@gmail.com>
2021-03-15 21:51:21 +0200

    CI Fixes

--- CI Scripts/dotnet-setup.ps1
+++ CI Scripts/dotnet-setup.ps1
@@ -1,4 +1,5 @@
 function InstallSDK {
+    echo "Installing dotnet SDK"
     wget "https://dotnetcli.azureedge.net/dotnet/Sdk/5.0.200/dotnet-sdk-5.0.200-win-x64.exe" -outfile "dotnet-setup.exe";
     $pathvargs = { .\dotnet-setup.exe /install /quiet /norestart };
     Invoke-Command -ScriptBlock $pathvargs
@@ -6,6 +7,7 @@ function InstallSDK {
 }
 
 function InstallRuntime {
+    echo "Installing dotnet Runtime"
     wget "https://download.visualstudio.microsoft.com/download/pr/dff39ddb-b399-43c5-9af0-04875134ce04/1c449bb9ad4cf75ec616482854751069/dotnet-hosting-5.0.3-win.exe" -outfile "dotnet-hosting-setup.exe";
     $pathvargs = { .\dotnet-hosting-setup.exe /install /quiet /norestart };
     Invoke-Command -ScriptBlock $pathvargs
